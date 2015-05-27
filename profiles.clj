68c47929fbad2b0373cc3aa373fb36359d03ad7a
pnaranja <paul.naranja@gmail.com>
2015-05-27 16:56:39 -0700

    Initial commit.  Includes nrepl and Utra

---
+++ profiles.clj
@@ -0,0 +1,8 @@
+{:user
+  {
+  :java-cmd "C:\\Program Files (x86)\\Java\\jdk1.8.0_31\\bin\\java.exe"
+  :plugins [[cider/cider-nrepl "0.8.1"]
+            [venantius/ultra "0.3.3"] 
+            ]
+   }
+ }
