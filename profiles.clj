bfa664f183589182b0187b5ba3f699cb0eb849dd
pnaranja <paul.naranja@gmail.com>
2015-06-11 09:26:51 -0700

    Added eastwood for linting

--- profiles.clj
+++ profiles.clj
@@ -2,7 +2,8 @@
   {
   :java-cmd "C:\\Program Files (x86)\\Java\\jdk1.8.0_31\\bin\\java.exe"
   :plugins [[cider/cider-nrepl "0.8.1"]
-            [venantius/ultra "0.3.3"] 
+            [venantius/ultra "0.3.3"]
+            [jonase/eastwood "0.2.1"] 
             ]
    }
  }
