730da7be531720664198485d67aefd4caded8c6a
pnaranja <paul.naranja@gmail.com>
2015-06-25 02:11:03 -0700

    Upgraded Clojure and nrepl versions

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,9 @@
 {:user
   {
-  :java-cmd "C:\\Program Files (x86)\\Java\\jdk1.8.0_31\\bin\\java.exe"
-  :plugins [[cider/cider-nrepl "0.8.1"]
-            [venantius/ultra "0.3.3"]
-            [jonase/eastwood "0.2.1"] 
-            ]
+  :java-cmd "C:\\Program Files (x86)\\Java\\jdk1.8.0_45\\bin\\java.exe"
+  :dependencies [[org.clojure/tools.nrepl "0.2.10"]]
+  :plugins [[org.clojure/clojure "1.7.0-RC1"]
+            [cider/cider-nrepl "0.9.1"]
+            [venantius/ultra "0.3.3"] ]
    }
  }
